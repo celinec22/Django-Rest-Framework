ede718ca6483c3eee10d57c7801e1fc887fb03f9
Celine Chahine <chahineceline@gmail.com>
2025-01-17 14:32:04 -0500

    header, content, params

--- drf/backend/api/views.py
+++ drf/backend/api/views.py
@@ -1,4 +1,18 @@
+import json
 from django.http import JsonResponse
 
 def api_home(request, *arges, **kwargs):
-    return JsonResponse({"message": "Hi there, this is your django API Response!"})
+    # request -> HttpRequest -> Django
+    print(request.GET) # url Query params
+    body = request.body #byte string of json data
+    data= {}
+    try:
+        data = json.loads(body) #string of json data -> python dictionary
+    except:
+        pass
+    print(data)
+    data['headers']= dict(request.headers)
+    data['content_type']= request.content_type
+    data['params']= dict(request.GET)
+
+    return JsonResponse(data)
--- drf/py_client/basic.py
+++ drf/py_client/basic.py
@@ -4,7 +4,7 @@ import requests
 # endpoint ="https://httpbin.org/status/200/"
 # endpoint ="https://httpbin.org/anything1234"
 endpoint = "http://localhost:8000/api/"
-get_response =requests.get(endpoint) 
+get_response =requests.get(endpoint, params={"abc": 123}, json = {"query": "Hello there"}) # this defaults in the case the server does not respond
 
-print(get_response.json()['message'])
-print(get_response.status_code)
+print(get_response.json())
+# print(get_response.status_code)
